88d5a71327edc5c9064899271884c49959cbadc6
owusuvictorjnr <owusuvictor80@gmail.com>
2024-12-09 19:04:28 +0000

    server

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,27 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors")
+import dotenv from "dotenv";
+import express from "express";
+import { json } from "body-parser";
+import cors from "cors";
 
+import authRoutes from "./routes/auth";
+import packagesRoutes from "./routes/packages";
+import ridersRoutes from "./routes/riders";
+import hubsRoutes from "./routes/hubs";
+import statsRoutes from "./routes/stats";
+
+dotenv.config();
 const app = express();
 
+app.use(json());
 app.use(cors());
-app.use(express.json());
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Backend up and running");
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/packages", packagesRoutes);
+app.use("/api/riders", ridersRoutes);
+app.use("/api/hubs", hubsRoutes);
+app.use("/api/dashboard/stats", statsRoutes);
 
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Backend running on http://localhost:${PORT}`);
 });
